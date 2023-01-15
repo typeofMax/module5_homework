c80faa83aba01667c18372c56199760584a99717
typeofMax <work@typeofmax.ru>
2023-01-15 10:15:11 +0300

    Complete task

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-c
+const someMap = new Map();
+
+someMap.set('a', 1);
+someMap.set('b', 2);
+someMap.set(true, 3);
+someMap.set(1, 4);
+someMap.set(null, 5);
+someMap.set(undefined, 6);
+
+for (let element of someMap) {
+    console.log(`Ключ - ${element[0]}, значение - ${element[1]}`);
+}
